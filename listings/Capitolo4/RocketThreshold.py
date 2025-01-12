5ef35e193d07dcca940da28e6e772e8ce5d6bba0
FRANCESCOFIASCHI1 <131721746+FRANCESCOFIASCHI1@users.noreply.github.com>
2025-01-12 23:00:59 +0100

    update: tesi scrittura

---
+++ listings/Capitolo4/RocketThreshold.py
@@ -0,0 +1,22 @@
+from rocket_functions import generate_kernels, apply_kernels
+
+def detect_anomalies_with_threshold(scores, threshold):
+    return (scores > threshold).astype(int)
+
+# Genera kernel convoluzionali casuali
+input_length = X_train.shape[1]
+num_kernels = 10000
+kernels = generate_kernels(input_length, num_kernels)
+
+# Applica i kernel alle serie temporali
+features_train = apply_kernels(X_train2, kernels)
+features_test = apply_kernels(X_test2, kernels)
+
+# Sintesi delle caratteristiche per esempio
+anomaly_scores_train = np.mean(features_train, axis=1)  # Media
+anomaly_scores_test = np.mean(features_test, axis=1)  # Media
+
+# Rilevamento delle anomalie
+threshold = np.percentile(anomaly_scores_train , 95)
+anomaly_labels_train = detect_anomalies_with_threshold(anomaly_scores_train , threshold)
+anomaly_labels_test = detect_anomalies_with_threshold(anomaly_scores_test , threshold)
